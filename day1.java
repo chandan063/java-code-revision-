03d0987285becb1bd83068955285c4cc7bd987fd
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-09-16 16:01:09 +0000

    first day

---
+++ day1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class day1{
+    public static int linearSearch(int number[],int key){
+        for(int i=0;i<number.length;i++){
+            if(number[i]==key){
+                return i;
+            }
+        }
+        return -1; //we use -1 if we dont't found the desired value which we are looking for
+    }
+
+    public static void main(String[] args){
+        int number[]={22,33,45,65,12,1,33};
+        // int key=65;
+        int key1=99;
+        int index=linearSearch(number,key1);
+        if(index==-1){
+            System.out.println("Not Found");
+        }
+        else System.out.println("Key found at:"+index);
+    }
+}
