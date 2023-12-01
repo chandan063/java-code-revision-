390337b5583a0f8b2a2d5b3f249199b72a57418c
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-12-01 19:10:09 +0530

    Create question.java
    StringBuilder uses question

---
+++ question.java
@@ -0,0 +1,28 @@
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+// A string is represented by an array if the array elements concatenated in order forms the string.
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
+// Example 2:
+
+// Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+// Output: false
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder str1=new StringBuilder();
+        StringBuilder str2=new StringBuilder();
+
+        for(String word:word1) str1.append(word);
+        for(String word:word2) str2.append(word);
+        String s1=str1.toString();
+        String s2=str2.toString();
+        if(s1.equals(s2)) return true;
+        else return false;
+    }
+   
+}
