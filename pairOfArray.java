81473f8efda08a85a956c9770e05da506de03436
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-12-09 16:03:09 +0530

    Update pairOfArray.java
    remove duplicate function added

--- pairOfArray.java
+++ pairOfArray.java
@@ -12,12 +12,27 @@ public class Program
         }
         System.out.println("total no of pairs are:"+count);
     }
-    
+    public static int removeDuplicate(int a[]){
+        if(a.length==0){return 0;}
+        int k=0;
+        for(int i=1;i<a.length;i++){
+            if(a[i]!=a[i-1]){
+                a[k]=a[i];
+                k++;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        return k;
+    }
     public static void main(String[] args) {
-		int a[]={22,55,56,57,88};
+		int a1[]={22,55,56,57,88};
+        int a[]={1,1,1,3,4,4,5,6,6,6,6};
         int key=10;
-        pairs(a);
-        
+        pairs(a1);
+        System.out.println(removeDuplicate(a));
 	}
     
 }
@@ -31,4 +46,7 @@ public class Program
 [57,88]
 
 total no of pairs are:10
+
+3 4 5 6 4 4 5 6 6 6 6 
+4
   */
