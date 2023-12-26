4ceefef1e09cdddc582230b3fec0857aa69ce237
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-12-26 11:04:32 +0000

    time-complexity tutorial

---
+++ timeComplexity/BigO.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BigO{
+    public static void main(String args[]){
+        int a[]={2,3,4,5};
+        // if array is sorted then 
+        int largest=a[a.length-1];
+        System.out.println(largest);
+        
+    }
+ }
