5c9d5a8095c8151263d4fb87dda7d23d9e2fbf34
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-12-27 09:32:41 +0000

    time-complexity in loop

---
+++ timeComplexity/loop.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//finding the time -complexity of the simple loop
+for(int i=0;i<n;i++){
+    //constant work
+    //is done in this loop
+
+    //to find the TC we need to find the operation happening here 
+    //means how many time the loop runs
+    // remove the constant 
+    O(n)==time-complexity;
+}
+
+// NESTED LOOP
+
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+         //constant work
+        //is done in this loop
+        TC=O(n^2);
+    }
+}
